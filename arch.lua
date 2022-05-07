c02ad32c8ad44666da464e9cd81cda248511a10c
keroeslux <104093192+keroeslux@users.noreply.github.com>
2022-05-07 23:29:05 +0000

    Update arch.lua

--- arch.lua
+++ arch.lua
@@ -5,10 +5,12 @@ function partition(drive)
   local partitions = function(drive)
     local timer = true
     while true do
-      print("Enter size of drive ( exit to finish | eg +1M +1G ): ")
+      print("Enter size of drive ( exit to finish | fill to fill the rest of drive | eg +1M +1G ): ")
       siz = io.read()
       if siz == "exit" then
         break
+      elseif siz == "fill" then
+        os.execute("sudo echo -e 'n\np\n\n\n\nw' | fdisk /dev/"..drive)
       end
       os.execute("sudo echo -e 'n\np\n\n\n"..siz.."\nw' | fdisk /dev/"..drive)
     end
@@ -17,6 +19,11 @@ function partition(drive)
   partitions(drive)
 end
 print("NOTE THIS WILL ERASE ALL DATA")
-print("Enter the name of the drive you want to use: ")
+print("Enter the name of the drive you want to use (ls to show all drives): ")
 local drive = io.read()
-partition(drive)
+if drive == "ls" then
+  os.execute("lsblk")
+  partition(drive)
+else
+  partition(drive)
+end
