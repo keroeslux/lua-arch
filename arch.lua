a2bb8e24e0250dcf8143a16ad9263b5f74e3123e
keroeslux <104093192+keroeslux@users.noreply.github.com>
2022-05-07 23:49:29 +0000

    Update arch.lua

--- arch.lua
+++ arch.lua
@@ -4,16 +4,33 @@ function partition(drive)
   end
   local partitions = function(drive)
     local timer = true
-    while true do
+    while timer == true do
       print("Enter size of drive ( exit to finish | fill to fill the rest of drive | eg +1M +1G ): ")
       siz = io.read()
       if siz == "exit" then
         break
       elseif siz == "fill" then
-        os.execute("sudo echo -e 'n\np\n\n\n\nw' | fdisk /dev/"..drive)
-        break
+        os.execute("sudo echo -e 'n\np\n\n\n\nw' | fdisk /dev/"..drive.." > /dev/null")
       end
-      os.execute("sudo echo -e 'n\np\n\n\n"..siz.."\nw' | fdisk /dev/"..drive)
+      os.execute("sudo echo -e 'n\np\n\n\n"..siz.."\nw' | fdisk /dev/"..drive.." > /dev/null")
+    end
+  end
+  local formatting = function(drive)
+    print("Enter the partition to format (list to show all partitions | type to show all supported fs formats | exit to finish): ")
+    local part = io.read()
+    print("Enter the fs format to use: ")
+    local typ = io.read()
+    local timer = true
+    while timer == true do
+      if part == "list" then
+        os.execute("sudo lsblk /dev/"..drive)
+      elseif part == "type" then
+        os.execute("vfat,\nntfs,\next4,\next3,\nxfs,\nbtrfs")
+      elseif part == "exit" then
+        break
+      else
+        os.execute("mkfs."..typ.." /dev/"..drive.."/"..part)
+      end
     end
   end
   reset(drive)
